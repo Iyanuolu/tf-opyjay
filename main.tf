f3a701043f1755f9b0cc12a5c0b7df3c9992c5cb
Opy Jay <seunmalissa@gmail.com>
2026-09-14 21:02:48 -0400

    my first commit

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "my_server" {
+  ami = "ami-06f5453d29c62393b"
+  count = 1
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "opyjay-tf-server"
+  }
+}
